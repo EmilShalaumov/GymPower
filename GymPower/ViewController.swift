f857872c81ae24f242a2467c6509be3bf893cfba
Эми <emilsalaumov@macbook-pro-emil.local>
2018-11-16 21:57:18 +0300

    Initial Commit

---
+++ GymPower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymPower
+//
+//  Created by Эмиль Шалаумов on 16/11/2018.
+//  Copyright © 2018 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
